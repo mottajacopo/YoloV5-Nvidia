9eea4171ddd7952c48cfd26ec8d771ed0a6e2a8b
mottajacopo <mottajacopo@gmail.com>
2023-01-18 18:09:11 +0000

    fix

--- run_triton.sh
+++ run_triton.sh
@@ -12,4 +12,4 @@ cp tensorrtx/yolov5/build/yolov5l.engine triton-deploy/models/yolov5l/1/model.pl
 cp tensorrtx/yolov5/build/yolov5x.engine triton-deploy/models/yolov5x/1/model.plan
 cp tensorrtx/yolov5/build/libmyplugins.so triton-deploy/plugins/
 
-docker run --gpus "device=0" --name tritonserver-22.07 --rm -it --shm-size=1g --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -p8220:8000 -p8221:8001 -p8222:8002 -v$(pwd)/triton-deploy/models:/models -v$(pwd)/triton-deploy/plugins:/plugins --env LD_PRELOAD=/plugins/libmyplugins.so nvcr.io/nvidia/tritonserver:22.07-py3 tritonserver --model-repository=/models --strict-model-config=false --grpc-infer-allocation-pool-size=16
+docker run --gpus "device=0" --name tritonserver-22.07 --rm -it --shm-size=1g --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -p8220:8000 -p8221:8001 -p8222:8002 -v $(pwd)/triton-deploy/models:/models -v$(pwd)/triton-deploy/plugins:/plugins --env LD_PRELOAD=/plugins/libmyplugins.so nvcr.io/nvidia/tritonserver:22.07-py3 tritonserver --model-repository=/models --strict-model-config=false --grpc-infer-allocation-pool-size=16
