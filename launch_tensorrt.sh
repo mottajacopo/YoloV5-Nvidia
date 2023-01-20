33c9b79c18e9c6c71a345da398799c94c29303fe
mottajacopo <mottajacopo@gmail.com>
2023-01-20 15:51:26 +0100

    Update launch_tensorrt.sh

--- launch_tensorrt.sh
+++ launch_tensorrt.sh
@@ -1,3 +1,3 @@
 model=$1
 
-sudo docker run --name=yolov5_trt_convert --rm -it --gpus '"device=0"' -v ${PWD}:/workspace tensorrt-22.07-py3-opencv4:latest convert_trt.sh $model
+docker run --name=yolov5_trt_convert --rm -it --gpus '"device=0"' -v ${PWD}:/workspace tensorrt-22.07-py3-opencv4:latest convert_trt.sh $model
